8b9ffd53c91d55334be4e3d71264c683b72c912a
mikezcc <zcc_zudiac@hotmail.com>
2016-03-14 14:34:56 -0400

    Added function to check single assignment

--- pype/semantic_analysis.py
+++ pype/semantic_analysis.py
@@ -4,7 +4,7 @@ class PrettyPrint(ASTVisitor):
   def __init__(self):
     pass
   def visit(self, node):
-    pass # TODO
+    print (node.__class__.__name__)
 
 class CheckSingleAssignment(ASTVisitor):
   def __init__(self):
