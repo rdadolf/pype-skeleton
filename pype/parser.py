322656316a42a403144c2077fb33a90c013db76e
mikezcc <zcc_zudiac@hotmail.com>
2016-03-14 11:32:30 -0400

    Updated parser file

--- pype/parser.py
+++ pype/parser.py
@@ -1,7 +1,7 @@
 import ply.yacc
 
-from .lexer import tokens,reserved
-from .ast import *
+from lexer import tokens,reserved
+from ast import *
 
 # Here's an example production rule which constructs an AST node
 def p_program(p):
@@ -23,49 +23,121 @@ def p_statement_list(p):
 # TODO Implement production rules for all other grammar rules and construct a
 #      full AST.
 
-# TODO
+def p_import_statement(p):
   r'import_statement : LPAREN IMPORT ID RPAREN'
-# TODO
+  print ("import",p[3])
+  p[0] = ASTImport(p[3])
+
+def p_component(p):
   r'''component : LBRACE ID expression_list RBRACE'''
-# TODO
+  print ("Component", p[2], p[3])
+  p[0] = ASTComponent(p[2], p[3])
+
+def p_expression_list(p):
   r'''expression_list : expression_list expression
                       | expression'''
-# TODO
+  if len(p)>2:
+    p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [p[1]]
+
+
+def p_expression_input(p):
   r'''expression : LPAREN INPUT declaration_list RPAREN
                  | LPAREN INPUT RPAREN'''
-# TODO
+  if len(p)>3:
+    print ("input ",p[3])
+    p[0] = ASTInputExpr(p[3])
+  else :
+    p[0] = ASTInputExpr([])
+
+def p_expression_output(p):
   r'''expression : LPAREN OUTPUT declaration_list RPAREN
                  | LPAREN OUTPUT RPAREN'''
-# TODO
+  if len(p)>3:
+      p[0] = ASTOutputExpr(p[3])
+  else :
+      p[0] = ASTOutputExpr([])
+
+def p_declaration_list(p):
   r'''declaration_list : declaration_list declaration
                        | declaration'''
-# TODO
+
+  if len(p)>2:
+    print ("Declaration list",p[0],p[1],p[2])
+    p[0] = p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [p[1]]
+
+def p_declaration(p):
   r'''declaration : LPAREN type ID RPAREN
                   | ID'''
-# TODO
+  #print ("Declare",len(p),p[0],p[1],p[2],p[3],p[4])
+  if len(p)>2:
+    p[0] = ASTID(p[3], p[2])
+  else :
+    p[0] = ASTID(p[1])
+
+def p_type(p):
   r'''type : ID'''
-# TODO
+  p[0] = ASTID(p[1]) #not sure
+
+def p_expression_assign(p):
   r'''expression : LPAREN ASSIGN ID expression RPAREN'''
-# TODO
+  print (":=",p[3],p[4])
+  p[0] = ASTAssignmentExpr(p[3], p[4])
+
+def p_expression_parameter_list(p):
   r'''expression : LPAREN ID parameter_list RPAREN
                  | LPAREN ID RPAREN'''
-# TODO
+  #TODO
+  if len(p)>4:
+    print ("Eval expr",p[2],p[3])
+    p[0] = ASTEvalExpr(p[2],p[3])
+  else:
+    p[0] = ASTEvalExpr(p[2], [])
+
+def p_expression_add(p):
   r'''expression : LPAREN OP_ADD parameter_list RPAREN'''
-# TODO
+  p[0] = ASTEvalExpr(p[2], p[3])
+
+def p_expression_sub(p):
   r'''expression : LPAREN OP_SUB parameter_list RPAREN'''
-# TODO
+  p[0] = ASTEvalExpr(p[2], p[3])
+
+def p_expression_mul(p):
   r'''expression : LPAREN OP_MUL parameter_list RPAREN'''
-# TODO
+  p[0] = ASTEvalExpr(p[2], p[3])
+
+def p_expression_div(p):
   r'''expression : LPAREN OP_DIV parameter_list RPAREN'''
-# TODO
+  p[0] = ASTEvalExpr(p[2], p[3])
+
+def p_expression_id(p):
   r'''expression : ID'''
-# TODO
+  p[0] = ASTID(p[1])
+
+def p_expression_num(p):
   r'''expression : NUMBER'''
-# TODO
+  p[0] = ASTLiteral(p[1])
+
+def p_expression_str(p):
   r'''expression : STRING'''
-# TODO
+  p[0] = ASTLiteral(p[1]) # not sure too....
+
+def p_parameter_list(p):
   r'''parameter_list : parameter_list expression
                      | expression'''
+  print ("param list:",len(p),p[0],p[1])
+  if len(p)>2:
+    p[0] = p[1].append(p[2])
+    p[0] = p[1]
+  else:
+    p[0] = [p[1]]
+
+
 
 # TODO: Write an error handling function. You should attempt to make the error
 #       message sensible. For instance, print out the line and column numbers to
@@ -74,6 +146,10 @@ def p_statement_list(p):
 #       If you're interested, read section 6.8, but it requires a fairly deep
 #       understanding of LR parsers and the language specification.
 def p_error(p):
-  pass
-# start = 'program'
-# parser = ply.yacc.yacc() # To get more information, add debug=True
+  # print("Illegal character '%s'" % p.value[0], "Error Line", t.lexer.lineno, "Error Column", find_column(data,t))
+  # t.lexer.skip(1)
+  return 0
+
+start = 'program'
+parser = ply.yacc.yacc(debug=True) # To get more information, add debug=True
+
