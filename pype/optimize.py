1c31b70009efbe0bbeb9a887d3fdf484b91dbeff
Bob Adolf <rdadolf@gmail.com>
2016-04-01 11:21:01 -0400

    Updates for Lab 25.

--- pype/optimize.py
+++ pype/optimize.py
@@ -14,6 +14,12 @@ class FlowgraphOptimization(Optimization):
   May NOT add or remove flowgraphs.'''
   pass
 
+class TopologicalFlowgraphOptimization(Optimization):
+  '''Called on each flowgraph in a FGIR, in dependent order.
+
+  Components which are used by other components will be called first.'''
+  pass
+
 class NodeOptimization(Optimization):
   '''Called on each node in a FGIR.
 
@@ -62,3 +68,41 @@ class DeadCodeElimination(FlowgraphOptimization):
   def visit(self, flowgraph):
     # TODO: implement this
     return flowgraph
+
+class InlineComponents(TopologicalFlowgraphOptimization):
+  '''Replaces every component invocation with a copy of that component's flowgraph.
+
+  Topological order guarantees that we inline components before they are invoked.'''
+  def __init__(self):
+    self.component_cache = {}
+
+  def visit(self, flowgraph):
+    for (cnode_id, cnode) in [(nid,n) for (nid,n) in flowgraph.nodes.items() if n.type==FGNodeType.component]:
+      target = self.component_cache[cnode.ref]
+      # Add a copy of every node in target flowgraph
+      id_map = {} # maps node id's in the target to node id's in our flowgraph
+      for tnode in target.nodes.values():
+        if tnode.type==FGNodeType.input or tnode.type==FGNodeType.output:
+          newtype = FGNodeType.forward
+        else:
+          newtype = tnode.type
+        n = flowgraph.new_node(newtype, ref=tnode.ref)
+        id_map[tnode.nodeid] = n.nodeid
+      # Connect all copies together
+      for tid,tnode in target.nodes.items():
+        flowgraph.nodes[id_map[tid]].inputs = [id_map[i] for i in tnode.inputs]
+      # Link inputs of cnode to inputs of target flowgraph
+      for cnode_input,targ_input in zip(cnode.inputs, target.inputs):
+        flowgraph.nodes[id_map[targ_input]].inputs = [cnode_input]
+      # Link output of target flowgraph to outputs of cnode
+      for oid,onode in flowgraph.nodes.items():
+        if cnode_id in onode.inputs:
+          onode.inputs[onode.inputs.index(cnode_id)] = id_map[target.outputs[0]]
+      # Remove all other references to cnode in flowgraph
+      del flowgraph.nodes[cnode_id]
+      victims = [s for s,nid in flowgraph.variables.items() if nid==cnode_id]
+      for v in victims:
+        del flowgraph.variables[v]
+    self.component_cache[flowgraph.name] = flowgraph
+    return flowgraph
+
--- pype/pipeline.py
+++ pype/pipeline.py
@@ -3,9 +3,12 @@ from .parser import parser
 from .ast import *
 from .semantic_analysis import CheckSingleAssignment, CheckSingleIOExpression, CheckUndefinedVariables
 from .translate import SymbolTableVisitor, LoweringVisitor
+from .optimize import *
+from .pcode import PCodeGenerator
 
 class Pipeline(object):
   def __init__(self, source):
+    self.pcodes = {}
     with open(source) as f:
       self.compile(f)
 
@@ -27,5 +30,12 @@ class Pipeline(object):
     # Optimization
     ir.flowgraph_pass( AssignmentEllision() )
     ir.flowgraph_pass( DeadCodeElimination() )
+    ir.topological_flowgraph_pass( InlineComponents() )
 
-    return ir
+    # PCode Generation
+    pcodegen = PCodeGenerator()
+    ir.flowgraph_pass( pcodegen )
+    self.pcodes = pcodegen.pcodes
+
+  def __getitem__(self, component_name):
+    return self.pcodes[component_name]
